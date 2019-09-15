f4e82666b4f381ab003b065a3db45c6b75a25fc9
Sergey P <pozhilov@gmail.com>
2019-09-15 12:30:37 +0300

    Styling quote action buttons

--- src/components/quotes/Quote.js
+++ src/components/quotes/Quote.js
@@ -5,9 +5,13 @@ import { editQuote, viewQuote, addQuote } from "../../redux/actions";
 import './Quotes.scss';
 
 const Quote = ({ quote, editQuote, viewQuote }) => (
-    <div className="quote" onClick={() => viewQuote(quote.id)}>
-      {quote.body}
-    </div>
+  <div className="quote">
+    <span className="quote__body" onClick={() => viewQuote(quote.id)}>{quote.body}</span>
+    <span className="quote__actions">
+      <button type="button" className="quote-action quote-action__edit">Edit</button>
+      <button type="button" className="quote-action quote-action__delete">Delete</button>
+    </span>
+  </div>
 );
 
 export default connect(null, { editQuote, viewQuote, addQuote })(Quote);
