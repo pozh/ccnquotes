f232e344651b7c59d3191a7d68f6cc772daea63d
Sergey P <pozhilov@gmail.com>
2019-09-15 13:15:32 +0300

    Quote - delete action

--- src/components/quotes/Quote.js
+++ src/components/quotes/Quote.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { editQuote, viewQuote, addQuote } from "../../redux/actions";
+import { editQuote, viewQuote, deleteQuote } from "../../redux/actions";
 
 import './Quotes.scss';
 
-const Quote = ({ quote, editQuote, viewQuote }) => (
+const Quote = ({ quote, editQuote, viewQuote, deleteQuote }) => (
   <div className="quote">
     <span className="quote__body" onClick={() => viewQuote(quote.id)}>{quote.body}</span>
     <span className="quote__actions">
-      <button type="button" className="quote-action quote-action__edit">Edit</button>
-      <button type="button" className="quote-action quote-action__delete">Delete</button>
+      <button type="button" className="quote-action quote-action__edit"
+              onClick={() => editQuote(quote.id)}>Edit</button>
+      <button type="button" className="quote-action quote-action__delete"
+              onClick={() => deleteQuote(quote.id)}>Delete</button>
     </span>
   </div>
 );
 
-export default connect(null, { editQuote, viewQuote, addQuote })(Quote);
+export default connect(null, { editQuote, viewQuote, deleteQuote })(Quote);
